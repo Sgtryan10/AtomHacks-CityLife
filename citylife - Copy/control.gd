853489d6575577d88bdbe458b3c9d1d1258af4dd
IamRyanandIamHomo <sh99098223@gmail.com>
2025-04-26 15:54:55 -0400

    sbc

---
+++ citylife - Copy/control.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	print("Start pressed") # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	print("Settings pressed")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
