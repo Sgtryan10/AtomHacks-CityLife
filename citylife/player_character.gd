67ad0a8bafecdc947c897e60d175fdde6c477844
Sgtryan10 <67563290+Sgtryan10@users.noreply.github.com>
2025-04-26 13:34:10 -0400

    testing2
    testing2

---
+++ citylife/player_character.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+## Speed in pixels per second.
+@export_range(0, 1000) var speed := 100
+
+func _physics_process(delta):
+	get_player_input()
+	move_and_slide()
+
+func get_player_input() -> void:
+	var vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = vector * speed
